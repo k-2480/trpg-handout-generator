dd50e002080f27a1c9d1e1fd84eeb13f415c4639
k-2480 <khei24.work80@gmail.com>
2024-12-09 23:35:27 +0900

    #14 fix: xmlのエスケープ処理を追加

--- card.xml.js
+++ card.xml.js
@@ -1,24 +1,30 @@
-function createCardXml(
-    frontImageFileName,
-    backImageFileName,
-    frontName,
-    backName,
-    backDetail
-) {
+function escapeXml(escapeStr) {
+    let replaceMap = {
+        "<": "&quot;",
+        ">": "&apos;",
+        "'": "&lt;",
+        '"': "&gt;",
+        "&": "&amp;",
+    };
+    return escapeStr.replace(/"'<>&/g, function (s) {
+        return replaceMap[s];
+    });
+}
+function createCardXml(frontImageFileName, backImageFileName, frontName, backName, backDetail) {
     return `<card location.name="table" location.x="500" location.y="500" posZ="0" state="1" rotate="0" owner="" zindex="0">
   <data name="card">
     <data name="image">
       <data type="image" name="imageIdentifier"></data>
-      <data type="image" name="front">${backImageFileName}</data>
-      <data type="image" name="back">${frontImageFileName}</data>
+      <data type="image" name="front">${escapeXml(backImageFileName)}</data>
+      <data type="image" name="back">${escapeXml(frontImageFileName)}</data>
     </data>
     <data name="common">
-      <data name="name">${frontName}</data>
+      <data name="name">${escapeXml(frontName)}</data>
       <data name="size">2</data>
     </data>
     <data name="detail">
-      <data name="${backName}">
-        <data name="" type="note">${backDetail}</data>
+      <data name="${escapeXml(backName)}">
+        <data name="" type="note">${escapeXml(backDetail)}</data>
       </data>
     </data>
   </data>
